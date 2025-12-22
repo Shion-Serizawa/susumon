5184be8231ddd96f25106c26100cad8268637c04
Shion-Serizawa <61482303+Shion-Serizawa@users.noreply.github.com>
2025-12-22 22:24:11 +0900

    GET /api/themesを作成

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	ssr: {
+		// Let Vite transform .prisma/client from CommonJS to ESM
+		noExternal: ['.prisma/client']
+	}
 });
